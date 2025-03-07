670481c1196e0dd0696108300c0e8a2c28b3dade
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-03-07 18:16:08 +0200

    added outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket used for raw invoice storage"
+  value       = aws_s3_bucket.invoice_bucket.id
+}
+
+output "dynamodb_table_name" {
+  description = "The name of the DynamoDB table for storing invoice data"
+  value       = aws_dynamodb_table.invoices.id
+}
+
+output "sns_topic_arn" {
+  description = "The ARN of the SNS topic for invoice processing alerts"
+  value       = aws_sns_topic.invoice_alerts.arn
+}
+
+output "lambda_function_arn" {
+  description = "The ARN of the Invoice Processor Lambda function"
+  value       = aws_lambda_function.invoice_processor.arn
+}
+
+output "api_gateway_invoke_url" {
+  description = "The Invoke URL for the API Gateway endpoint"
+  value       = aws_api_gateway_stage.prod.invoke_url
+}
+
